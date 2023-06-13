4893ac9b644c5bd00d7e0b6aa0102acba17e5193
Parichat <jungjinggg@icloud.com>
2023-06-13 17:20:11 +0100

    adding feature to ensure that the input is int 3rd commit

--- python_collections/tuesday_task/magic_number.py
+++ python_collections/tuesday_task/magic_number.py
@@ -6,23 +6,27 @@ import random
 magic_num = random.randint(1, 100)
 # print(magic_num)
 
-# Ask the user for a guess
-guessed_num = int(input("Enter a number between 1 - 100: "))
-
 # Check if user's guess was correct
-if guessed_num == magic_num:
-    print("Congratulation! You've guessed correctly")
-    #break
-elif guessed_num < magic_num:
-    print("The number is too low")
-    #continue
-elif guessed_num > magic_num:
-    print("The number is too high")
-    #continue
-else:
-    print("Unfortunately! You've got it wrong this time")
-    #break
+while True:
+    try:
+        guessed_num = int(input("Enter a number between 1 - 100: "))
+    except ValueError:
+        print("Please enter a number")
+        break
 
-# Give them the result
-print(f'\nThe magic number is {magic_num}')
+    if guessed_num == magic_num:
+        print("Congratulation! You've guessed correctly")
+        #break
+    elif guessed_num < magic_num:
+        print("The number is too low")
+        #continue
+    elif guessed_num > magic_num:
+        print("The number is too high")
+        #continue
+    else:
+        print("Unfortunately! You've got it wrong this time")
+        #break
+
+    # Give them the result
+    print(f'\nThe magic number is {magic_num}')
 
